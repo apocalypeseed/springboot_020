7a8edcb0508de3fb2733fd0333a87bb89c76bccd
lewt <lewt@lew.com>
2018-07-24 17:40:35 -0400

    Initial commit

---
+++ src/main/java/com/thomas/springboot_020/Springboot020Application.java
@@ -0,0 +1,12 @@
+package com.thomas.springboot_020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot020Application.class, args);
+	}
+}
